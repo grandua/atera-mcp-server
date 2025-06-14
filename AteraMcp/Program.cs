2d88631a6af9307070131de5e92c7fd088ce512a
Sergey <grand.ua@gmail.com>
2025-06-13 21:15:27 -0400

    Moved EchoTool.cs out

--- AteraMcp/Program.cs
+++ AteraMcp/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ModelContextProtocol.Server;
-using System.ComponentModel;
 
 namespace AteraMcp;
 
@@ -18,14 +16,4 @@ public class Program
         var host = builder.Build();
         await host.RunAsync();
     }
-}
-
-[McpServerToolType]
-public static class EchoTool
-{
-    [McpServerTool, Description("Echoes the message back to the client.")]
-    public static string Echo(string message) => $"Hello from C#: {message}";
-
-    [McpServerTool, Description("Echoes in reverse the message sent by the client.")] 
-    public static string ReverseEcho(string message) => new string(message.Reverse().ToArray());
 }
